1ee7b986994bceb586a384d1ffbea37b01dd4def
wendelsilverio <wendelsilverio@gmail.com>
2016-10-16 19:02:28 -0200

    Liberação do cross-domain

--- src/main/java/wendelsilverio/math/MathEvaluatorApplication.java
+++ src/main/java/wendelsilverio/math/MathEvaluatorApplication.java
@@ -2,11 +2,13 @@ package wendelsilverio.math;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins="https://wendelsilverio.github.io")
 @SpringBootApplication
 public class MathEvaluatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MathEvaluatorApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MathEvaluatorApplication.class, args);
+	}
 }
