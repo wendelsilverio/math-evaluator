ba4d867a1feedc6b9321d56ad67004a995216440
Wendel <wendelsilverio@users.noreply.github.com>
2016-10-16 18:04:22 -0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
 console.log('This would be the main JS file.');
+
+function getJSONP(url, success) {
+
+    var ud = '_' + +new Date,
+        script = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0] 
+               || document.documentElement;
+
+    window[ud] = function(data) {
+        head.removeChild(script);
+        success && success(data);
+    };
+
+    script.src = url.replace('callback=?', 'callback=' + ud);
+    head.appendChild(script);
+
+}
