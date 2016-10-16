0d08e3bb1646ae496949bf5d2f4f09edd8935457
wendelsilverio <wendelsilverio@gmail.com>
2016-10-16 16:31:50 -0200

    Versão inicial

---
+++ src/main/java/wendelsilverio/math/MathEvaluatorApplication.java
@@ -0,0 +1,12 @@
+package wendelsilverio.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathEvaluatorApplication.class, args);
+    }
+}
