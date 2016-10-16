c18bb9a7f22ca30b7effdfb70957ab316e03227f
Wendel <wendelsilverio@users.noreply.github.com>
2016-10-16 19:01:30 -0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 	var url = 'https://math-evaluator.herokuapp.com/health';
 	$.ajax({
-		type: 'GET',
+	  type: 'GET',
 	  url: url,
-	  dataType: "jsonp",
 	  success: function (data) {
 		console.log(data)
 		alert(data);
