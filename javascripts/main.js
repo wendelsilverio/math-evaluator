a351cb126b8278dd4b1b8d1040de368daa1f4aa1
wendelsilverio <wendelsilverio@gmail.com>
2016-11-06 20:11:18 -0200

    Rest call fixed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 	$('#btnCalculate').click(function(){
-		
+		var url = 'https://math-evaluator.herokuapp.com/me?f='+encodeURIComponent($('#txtFunction').val());
+		$.ajax({
+		  type: 'GET',
+		  url: url,
+		  success: function (data) {
+			$('#rest-result').append(data);
+		  }
+		});
 	});
-	var url = 'https://math-evaluator.herokuapp.com/me?f='+$('#txtFunction').val();
+	
+	var url = 'https://math-evaluator.herokuapp.com/health';
 	$.ajax({
 	  type: 'GET',
 	  url: url,
 	  success: function (data) {
-		$('#rest-content').append(data.status);
+		$('#rest-hb').append(data.status);
 	  }
 	});
 });
