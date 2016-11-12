feb781f5c9575ae122dc57a4195560ecd8fb2ff1
wendelsilverio <wendelsilverio@gmail.com>
2016-11-12 19:48:18 -0200

    Adjusted URL and prepared to receive the variables by REST

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,24 +1,39 @@
+/* Javascript hack */
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
 $(document).ready(function() {
 	
 	var host = 'https://math-evaluator.herokuapp.com';
+	//var host = 'http://localhost:8080';
 	
 	$('#btnCalculate').click(function(){
 
 		$('#rest-result').empty();
 		$('#calc-error').empty();
-		
-		var url = host+'/me?f='+encodeURIComponent($('#txtFunction').val());
+		$('#result-link').empty();
+		$('#result-link').attr('href', '');
+		$('.rest-url').hide();
+
+		var url = host+'/?f='+encodeURIComponent($('#txtFunction').val()).replaceAll('%26','&').replaceAll('%3D','=').replace(/ /g,'');
 		$.ajax({
 		  type: 'GET',
 		  url: url,
 		  success: function (data) {
-			  if(data.r) {
-				$('#rest-result').append(data.r);
+			  $('#result-link').append(url);
+			  $('#result-link').attr('href', url);
+			  $('.rest-url').show();
+			  if(data.result) {
+				
+				$('#rest-result').append(data.result);
 			  } else {
 				$('.error').show();
 				$('#calc-error').append(data.error);
 			  }
-		  }
+		  }     	
+				
 		});
 	});
 });
